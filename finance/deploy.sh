aeb5560eddb68a6f57e78da347a47f36f6d8ffe1
mawz <mawz@hey.com>
2025-01-20 18:49:47 -0800

    update deploy script

--- finance/deploy.sh
+++ finance/deploy.sh
@@ -1,10 +1,13 @@
+set -eo pipefail
+
 dir="/mnt/super-fly/shared/finances/scripts"
 
 chmod 600 access_url
 cp flake.nix "$dir"
 cp flake.lock "$dir"
-cp access_url "$dir"
 cp sfin2ledger.py "$dir"
 cp ledger_scheduler.py "$dir"
 cp commodity_updater.py "$dir"
+
+cp access_url "$dir"
 echo "Remember to \`chown finance-sync: access_url\` on the remote!"
